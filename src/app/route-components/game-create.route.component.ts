15b4b5f348dda2366c54181f349affec794be720
guillbdx <gpedelagrabe@gmail.com>
2017-05-04 22:19:53 +0200

    Game creation : choice between vs user and vs computer

--- src/app/route-components/game-create.route.component.ts
+++ src/app/route-components/game-create.route.component.ts
@@ -3,4 +3,20 @@ import { Component } from '@angular/core';
 @Component({
     templateUrl: './game-create.route.component.html',
 })
-export class GameCreateRouteComponent {};
+export class GameCreateRouteComponent {
+
+    formVsComputerIsVisible = false;
+
+    formVsUserIsVisible = false;
+
+    displayFormVsComputer() {
+        this.formVsComputerIsVisible = true;
+        this.formVsUserIsVisible = false;
+    }
+
+    displayFormVsUser() {
+        this.formVsComputerIsVisible = false;
+        this.formVsUserIsVisible = true;
+    }
+
+};
