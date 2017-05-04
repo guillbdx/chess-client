3d5486b1f97566235573af4442d6c23455ab473d
guillbdx <gpedelagrabe@gmail.com>
2017-05-04 10:51:16 +0200

    Core folder

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginRouteComponent }          from './route-components/login.route.component';
-import { RegisterRouteComponent }       from './route-components/register.route.component';
-import { ProfileRouteComponent }        from './route-components/profile.route.component';
-import { GamesRouteComponent }          from './route-components/games.route.component';
-import { GameRouteComponent }           from './route-components/game.route.component';
-import { GameCreateRouteComponent }     from './route-components/game-create.route.component';
-import { ChangePasswordRouteComponent } from './route-components/change-password.route.component';
+import { LoginRouteComponent }          from '../route-components/login.route.component';
+import { RegisterRouteComponent }       from '../route-components/register.route.component';
+import { ProfileRouteComponent }        from '../route-components/profile.route.component';
+import { GamesRouteComponent }          from '../route-components/games.route.component';
+import { GameRouteComponent }           from '../route-components/game.route.component';
+import { GameCreateRouteComponent }     from '../route-components/game-create.route.component';
+import { ChangePasswordRouteComponent } from '../route-components/change-password.route.component';
 
 const routes: Routes = [
   {
--- src/app/login.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template: '<h2>Login</h2>',
-})
-export class LoginRouteComponent {};
