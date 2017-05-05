f4cc6d4d41ed5e7361852a9cca0460422c56335c
guillbdx <gpedelagrabe@gmail.com>
2017-05-05 05:15:55 +0200

    Profile page

--- src/app/route-components/profile.route.component.ts
+++ src/app/route-components/profile.route.component.ts
@@ -1,6 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../entities/user.entity";
+import {Router} from "@angular/router";
+import {ChessApiClientService} from "../services/chess-api-client.service";
 
 @Component({
     templateUrl: './profile.route.component.html',
 })
-export class ProfileRouteComponent {};
+export class ProfileRouteComponent implements OnInit {
+
+    profile: User;
+
+    constructor(
+        private chessApiClient: ChessApiClientService
+    ) {}
+
+    ngOnInit() {
+        this.chessApiClient.getProfile()
+            .then(user => {
+                this.profile = user;
+                console.log(this.profile);
+            });
+    }
+
+};
