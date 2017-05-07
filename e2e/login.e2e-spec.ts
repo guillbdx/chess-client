5f6ba0fa25bda36ba3e66a4f233c6cc76bfd63cb
guillbdx <gpedelagrabe@gmail.com>
2017-05-07 18:25:11 +0200

    Test login with wrong credentials

--- e2e/login.e2e-spec.ts
+++ e2e/login.e2e-spec.ts
@@ -17,6 +17,25 @@ describe('Login feature', () => {
         expect(title).toEqual('Login');
     });
 
+    it('should display an error message when i try to log with wrong credentials', () => {
+
+        browser.get('/');
+
+        let usernameInput = element(by.css('input#username'));
+        let passwordInput = element(by.css('input#password'));
+        let submitButton = element(by.buttonText('Login'));
+
+        usernameInput.sendKeys('xxx');
+        passwordInput.sendKeys('yyy');
+        submitButton.click();
+
+        let alertErrorContainer = element(by.css('.alert-danger'));
+        let alertErrorContent = alertErrorContainer.getText();
+        expect(alertErrorContainer.isPresent()).toBeTruthy();
+        expect(alertErrorContent).toEqual('Wrong credentials');
+
+    });
+
     it('should drive me to Games page when i log with proper credentials', () => {
         browser.get('/');
 
