e31289832823b71294cfd58f69fd6e97a0ba5f7e
guillbdx <gpedelagrabe@gmail.com>
2017-05-09 11:16:03 +0200

    Chessboard wrapper

--- src/app/core/game.component.ts
+++ src/app/core/game.component.ts
@@ -11,8 +11,30 @@ export class GameComponent implements OnInit {
     @Input()
     game: Game;
 
+    unit: number;
+
     ngOnInit() {
         console.log(this.game);
+        this.setUnit();
+        this.resizeContainer();
+    }
+
+    setUnit() {
+        let containerWidth = document.getElementById('chessboard-wrapper-by-white').offsetWidth;
+        this.unit = containerWidth / 9;
+        if(window.innerWidth > window.innerHeight) { // Landscape mode
+            let containerHeight = window.innerHeight - 100;
+            this.unit = containerHeight / 11;
+        }
+    }
+
+    resizeContainer() {
+        document.getElementById('chessboard-wrapper-by-white').style.width = (9 * this.unit) + 'px';
+        document.getElementById('chessboard-wrapper-by-white').style.height = (11 * this.unit) + 'px';
+        let tds = document.querySelectorAll('td');
+        for(let h = 0; h < tds.length; h++) {
+            tds[h].style.height = this.unit + 'px';
+        }
     }
 
 }
