c7b85363a44e267c8ff6dd4b1743e02ad2f29cc7
guillbdx <gpedelagrabe@gmail.com>
2017-05-20 14:05:25 +0200

    Setup BabylonJS

--- src/app/route-components/login.route.component.ts
+++ src/app/route-components/login.route.component.ts
@@ -1,6 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-    templateUrl: './login.route.component.html',
+    //templateUrl: './login.route.component.html',
+    template: '<canvas id="canvas"></canvas>',
+    styles: ['#canvas {width: 400px;height: 400px;touch-action: none; border: solid 1px red;}']
 })
-export class LoginRouteComponent {}
+export class LoginRouteComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {
+
+        let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+
+        let engine = new BABYLON.Engine(canvas, true);
+
+        let scene = new BABYLON.Scene(engine);
+
+        let camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+
+        camera.setTarget(BABYLON.Vector3.Zero());
+
+        camera.attachControl(canvas, false);
+
+        let light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+        let sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+
+    }
+
+
+}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,7 @@
-/* SystemJS module definition */
+/// <reference path="babylon-declaration-types/babylon.2.5.d.ts" />
+/// <reference path="babylon-declaration-types/babylon.woodProceduralTexture.d.ts" />
+
 declare var module: NodeModule;
 interface NodeModule {
-  id: string;
+    id: string;
 }
